dc763e97bd2736d2933101be3ac9662929e2b3ca
krsna6 <kisna.philly@gmail.com>
2019-04-25 21:16:53 -0700

    algorithm and simulation experiments

---
+++ cp_ssc.m
@@ -0,0 +1,212 @@
+%--------------------------------------------------------------------------
+% TO DO - notation in KS notes
+%--------------------------------------------------------------------------
+% Krishna Somandepalli, Nov 2017
+%--------------------------------------------------------------------------
+% 
+function [C2, B, err_1, err_2, err_3] = cp_ssc(X, Y, affine, l)%,alpha,thr,maxIter)
+
+tol = 1e-6;
+
+% affine = false;
+maxiter = 1000;
+%num of samples = n; feature dim=d
+[d,n] = size(X);
+m = d+n;
+
+% find some params
+mu_z1 = computeLambda_mat(X);
+% mu_z2 = computeLambda_mat([X;Y]);
+mu_z2 = lansvd(sign([X;Y]), 1, 'L');
+
+alpha = 10;
+
+alpha1 = alpha/mu_z1;
+alpha2 = mu_z2;
+
+% set all mu
+mu1 = alpha1;
+mu2 = alpha;
+mu3 = 0.1/alpha2;
+
+% little lambda: l1 weighs ||C||_1, l2 weighs ||(X;Y)||_*
+% pick a lil lambda such that the weights on l1 and l* norm sum to 1 
+% l = 1000;
+l1 = (l/(1+l));
+l2 = (1/(1+l));
+
+% lagrange multipliers that go into trace
+L2 = zeros(n);
+L3 = zeros(m,n);
+
+% init
+C = zeros(n); % SSC
+B = zeros(m,n); % MC
+% need to update only the lower nxn!
+M = zeros(m,n); % MC
+
+% all entries that belong to cannot link pairs = function psi
+C_cannot = (Y<=-1);
+
+% function for soft thresholding - inputs matrix, threshold, size of the
+% matrix
+soft_threshold = @(K,beta,n)(max(0, abs(K) - (beta*ones(n)) ) .* sign(K));
+
+% inf norm error between vectorized matrices
+inf_norm_err = @(K1, K2)(max(max( abs(K1-K2) )));
+
+% update_param = []
+err_1 = 10*tol; err_2 = 10*tol; err_3 = 10*tol; err_4 = 10*tol;
+
+iter = 1;
+if (~affine)
+    
+    G = X'*X;
+    H = inv( mu1*G + mu2*eye(n) );
+    
+    while (err_1(iter) > tol && err_3(iter) > tol && iter < maxiter)
+        %update J
+        J = H * ((mu1*G) + (mu2*C) - L2);
+        J = J - (C_cannot .* J);
+        J = J - diag(diag(J)) ;
+
+        % update C - soft thresholding
+        C2 = soft_threshold( J + (L2/mu2), l1/mu2, n);
+%       C2 = max(0,(abs(J+L2/mu2) - (l1/mu2)*ones(n))) .* sign(J+(L2/mu2));
+        C2 = C2 - (C_cannot .* C2);
+        C2 = C2 - diag(diag(C2));
+
+        C = C2;
+        
+%         update_param = [update_param, ]        
+        % create data matrix for matrix completion
+        if iter==1
+            N = [X ; Y];
+        else
+            N = [X*J ; Y];
+        end
+        
+        % update R
+        % R = Y - F + L3/mu3
+        M(d+1:end, :) = Y - B(d+1:end,:) + (L3(d+1:end,:)/mu3);
+        % function phi
+        M(N~=0) = 0;
+               
+        % update B  -singular value soft thresholding
+        [U,S,V] = svd(N - M + (L3/mu3), 'econ');
+        % note that singular values are always positive!
+        % and singular values come sorted in matlab
+        diag_S = diag(S);
+        beta_2 = l2/mu3;
+        svp = length( find(diag_S >  beta_2) );
+        B = U(:,1:svp) * diag( diag_S(1:svp) - beta_2 ) * V(:,1:svp)';
+        
+        N_err = N - M - B;
+        
+        % update C_cannot
+        C_cannot = (B(d+1:end,:)<=-1);
+        
+        % update big lambda
+        L2 = L2 + mu2 * (J - C2);
+        L3 = L3 + mu3 * N_err; 
+        
+        % update mu3
+        rho_s = length(N~=0)/(m*n);
+        rho = 1.1 + 2.5*rho_s;
+        mu3 = rho*mu3;
+        
+        
+        % find errors in the SSC part
+        err_1(iter+1) = inf_norm_err(J,C2);
+        err_2(iter+1) = errorLinSys(X, J);
+        
+        % find errors in the MC part
+        err_3(iter+1) = norm(N_err, 'fro')/norm(N, 'fro');
+        if mod(iter, 50)==0
+            disp([iter, err_1(end), err_2(end), err_3(end), svp]);
+        end
+        iter = iter + 1;
+        
+%         subplot(121); imagesc(abs(C2)+abs(C2')); subplot(122); imagesc(B(d+1:end,:))
+%         pause(0.1)
+        
+    end
+    
+else
+    G = X'*X;
+    H = inv( mu1*G + mu2*eye(n) + 1*mu2*ones(n));
+    L4 = ones(1,n);
+    
+    while ((err_1(iter) > tol || err_4(iter) > tol) && err_3(iter) > tol...
+            && iter < maxiter)
+        %update J
+        J = H * ((mu1*G) + (1*mu2*C) - (1*L2) + (mu2*ones(n)) - (ones(n,1)*L4));
+        J = J - (C_cannot .* J);
+        J = J - diag(diag(J)) ;
+
+        % update C - soft thresholding
+        C2 = soft_threshold( J + (L2/mu2), l1/(1*mu2), n);
+%         C2 = max(0,(abs(J+L2/mu2) - (l1/mu2)*ones(n))) .* sign(J+(L2/mu2));
+        
+        C2 = C2 - (C_cannot .* C2);
+        C2 = C2 - diag(diag(C2));
+        C = C2;
+
+%         update_param = [update_param, ]
+        
+        % create data matrix for matrix completion
+        N = [X*J ; Y];
+%         imagesc(C_cannot);
+%         pause(0.1)
+        % update R
+        % R = Y - F + L3/mu3
+        M(d+1:end, :) = Y - B(d+1:end,:) + (L3(d+1:end,:)/mu3);
+        % function phi
+        M(N~=0) = 0;
+               
+        % update B  -singular value soft thresholding
+        [U,S,V] = svd(N - M + (L3/mu3), 'econ');
+        % note that singular values are always positive!
+        % and singular values come sorted in matlab
+        diag_S = diag(S);
+        beta_2 = l2/mu3;
+        svp = length( find(diag_S >  beta_2) );
+        B = U(:,1:svp) * diag( diag_S(1:svp) - beta_2 ) * V(:,1:svp)';
+        
+        N_err = N - M - B;
+        
+        % update C_cannot
+        C_cannot = (B(d+1:end,:)<=-1);
+        
+        % update big lambda
+        L2 = L2 + mu2 * (J - C2);
+        L3 = L3 + mu3 * N_err; 
+        L4 = L4 + mu2 * (ones(1,n)*J - ones(1,n));
+        % update mu3
+        rho_s = length(N~=0)/(m*n);
+        rho = 1.1 + rho_s;
+        mu3 = rho*mu3;
+
+        
+        
+        % find errors in the SSC part
+        err_1(iter+1) = inf_norm_err(J,C2);
+        err_2(iter+1) = errorLinSys(X, J);
+        err_4(iter+1) = inf_norm_err(ones(1,n)*J,ones(1,n));
+        
+        % find errors in the MC part
+        err_3(iter+1) = norm(N_err, 'fro')/norm(N, 'fro');
+        
+        if mod(iter, 10)==0
+            disp([iter, err_1(end), err_2(end), err_4(end), err_3(end), svp]);
+        end
+        iter = iter + 1;
+        
+        
+%         subplot(121); imagesc(abs(C2)+abs(C2')); subplot(122); imagesc(B(d+1:end,:))
+%         pause(0.1)
+    end
+    
+end
+
+end
---
+++ simulation_experiments_joint_cp_ssc.m
@@ -0,0 +1,188 @@
+%--------------------------------------------------------------------------
+% This routine evaluates the ability of the proposed outlier detection
+% scheme
+%
+%
+% This corresponds to Figure 12 of the paper
+% "a geometric analysis of subspace clustering with outliers".
+%
+%
+%--------------------------------------------------------------------------
+% by Mahdi Soltanolkotabi and Emmanuel Candes.
+%--------------------------------------------------------------------------
+
+
+addpath /home/krsna/Desktop/IDEA_clustering_constraint_prop/PROPACK_new2011
+clear all;
+close all;
+clc;
+rng('default')
+rng(320)
+
+% cvx_solver sdpt3
+% cvx_quiet(true) ;
+dvals = [ 10]%, 15, 20, 25, 30, 35, 40, 45, 8, 6]     ;
+cn_vals = [0.8] %, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4] ;
+% 
+% C_out           = cell( 1 , length(dvals)*length(cn_vals) ) ;
+% F_out           = cell( 1 , length(dvals)*length(cn_vals) ) ;
+% 
+% C_out_joint     = cell( 1 , length(dvals)*length(cn_vals) ) ;
+% F_out_joint     = cell( 1 , length(dvals)*length(cn_vals) ) ;
+% 
+
+
+% d = 5 ;
+sigma = 0.1;
+ind_n = 0 ;
+
+% \% of constraints
+tol = 1e-5;   maxiter = 500;
+% ambient dim = feature dim
+n = 50;
+% for n  = [50] %nvals                       % The dimension of the space
+for d = dvals
+    
+    num_subspaces = 10; %round(2*n/d);
+    %round( 2 * n / d )  ;         % number of subspaces you want to generate
+
+    
+    dim_subspaces = randi([d, (n/2)-1], 1, num_subspaces);
+%     5 * ones(1,num_subspaces) ;   % this a vector of dimensions of the subspaces
+                                                  % e.g. dim_subspaces(3)=2 ;
+                                                  % corresponds to the fact that the
+                                                  % third subspace has two degree's
+                                                  % of freedom (two base vectors in R^n)
+    
+        
+                                                  
+    Base_subspaces = cell(1, num_subspaces ) ;
+    
+                                                  % the i'th element of this cell is
+                                                  % an n * dim_subspaces(i) matrix
+                                                  % which is a base for the i'th
+                                                  % subspace
+    
+    num_points_on_subspace  = dim_subspaces.*randi([2,4], 1, num_subspaces);
+%     5*dim_subspaces(1)*ones(1,num_subspaces) ;
+    
+                                                  % number of points generated from
+                                                  % each subspace
+    
+    
+    N = sum( num_points_on_subspace ) ;           % total number of points
+
+%     num_outliers = round(N/6) ;                            % number of outliers
+
+
+% generating the subspaces
+
+    for kk = 1 : num_subspaces
+
+        Base_subspaces{kk}     = orth( randn( n , dim_subspaces(kk) ) ) ;
+
+    end
+
+
+% generating the points on the subspaces
+
+    X0 = zeros( n , N) ;
+    start_ind      = cumsum( num_points_on_subspace ) + 1 ;
+    start_ind(end) = [] ;
+    start_ind = [ 1 , start_ind ] ;
+    end_ind   = start_ind + num_points_on_subspace - 1 ;
+
+    for kk = 1 : num_subspaces
+
+        temp = randn(  dim_subspaces(kk) , num_points_on_subspace(kk) ) ;
+        temp = temp ./ repmat( sqrt(sum(temp.^2)) , size(temp,1) , 1 ) ;
+        X0( : , start_ind(kk):end_ind(kk)  ) = Base_subspaces{kk} * temp ;
+
+    end
+
+    X1 = X0 ./ repmat( sqrt(sum(X0.^2)) , size(X0,1) , 1 ) ;
+
+% % %     Add Outliers
+%     outliers = randn(n,num_outliers) ;
+%     outliers = outliers ./ repmat( sqrt(sum(outliers.^2)) , size(outliers,1) , 1 ) ;
+%     X1 = [ X0 , outliers ] ;
+%     clear X0;
+
+% %     add noise with sigma
+    N_  = randn(size(X1));
+    N_  = N_ ./ repmat( sqrt(sum(N_.^2)) , size(N_,1) , 1 ) ;
+    X  = X1 + sigma * N_ ;
+    X  = X ./ repmat( sqrt(sum(X.^2)) , size(X,1) , 1 ) ;
+    %make some room
+    clear X1;
+    clear N_;
+%     
+%     N = N+num_outliers ;
+
+    num_inliers = sum(num_points_on_subspace);
+
+    subspace_labels = [];
+    for i=1:length(num_points_on_subspace)
+        subspace_labels = [subspace_labels; i*ones(num_points_on_subspace(i),1)];
+    end
+%     subspace_labels = [subspace_labels; zeros(N-num_inliers,1)];
+    
+    % use 1 and 2 instead of -1 and 1
+    full_subspace_constraints = -1*ones(N,N);
+    for i=1:length(num_points_on_subspace)
+        full_subspace_constraints(start_ind(i):end_ind(i),start_ind(i):end_ind(i)) = 1;
+    end
+
+    
+	disp(['done generating data for d = ', num2str(d)])
+	disp('running each');
+	[C,err1,err2,err3] = admmOutlier_mat_func(X, true);
+    
+	C1 = C(1:N, :);
+	clear C;
+    for cn_x = cn_vals
+		ind_n = ind_n + 1 ;
+		[d,cn_x,ind_n]
+		num_constraints = cn_x ; %0.05;
+		R = num_subspaces;
+		cn = round(num_constraints*R*(sum(size(X))-R)); %round( ((num_inliers^2 - num_inliers)/2) * num_constraints);
+		
+	    %     sample pairwise constrtiants from non-outliers only!!
+		pc = ones(N, N); 
+		pc = triu(pc, 1); 
+		idx = find(pc>0); clear pc;
+		cn_all = numel(idx);
+		ind = randperm(cn_all); 
+		ind = idx(ind(1:cn)); clear indc; 
+
+	    %     take care that the indices are mapped to inliers only - not outliers
+		Y_incomplete = sparse(zeros(N,N));   
+		for kc = 1:cn
+		    i = mod(ind(kc)-1,N)+1;
+		    j = (ind(kc)-i)/N+1;
+		    Y_incomplete(i,j) = full_subspace_constraints(i,j);
+		    Y_incomplete(j,i) = full_subspace_constraints(j,i);
+		end;
+		disp('running joint');
+        
+        Data = [X; Y_incomplete];
+        [WF, E, ~] = inexact_alm_mc(Data, tol, maxiter);
+        F = WF(size(X,1)+1:end, :);
+        E = E(size(X,1)+1:end, :);
+	%         Data = [X; Y];
+	%         [WF, E, ~] = inexact_alm_mc(Data, tol, maxiter);
+        for l=[1e-3,1e-2,1e-1,1,10,100,1000]
+            l
+%             [C,WF, err1J, err2J, err3J, svp_all] = admmOutlier_nuc_norm(X, Y_incomplete, l);
+            [C,WF, err1J, err2J, err3J] = cp_ssc(X, Y_incomplete, false, l);
+
+            F_joint = WF(size(X,1)+1:end, :);
+            C_joint = C(1:N, :);
+% 
+%             save(['expt_lambda_d',num2str(d),'_cn', num2str(cn_x) ,'_l',num2str(l),'.mat'], 'dim_subspaces', ...
+%                 'num_points_on_subspace', 'subspace_labels', 'full_subspace_constraints',...
+%                 'X', 'F_joint', 'C_joint', 'err1J', 'err2J', 'err3J', 'err1',...
+%                  'err2', 'err3', 'C1', 'F', 'Y_incomplete', 'l')
+        end
+    end
+end
